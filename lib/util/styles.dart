5fa88560884cfc1daa5e74fea235a457f17b1b98
AhmedKaladl <info@eato.ly>
2024-01-13 01:33:53 +0200

    new

---
+++ lib/util/styles.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+const IBMPlexSansArabicRegular = TextStyle(
+  fontFamily: 'IBMPlexSansArabic',
+  fontWeight: FontWeight.w400,
+);
+
+const IBMPlexSansArabicMedium = TextStyle(
+  fontFamily: 'IBMPlexSansArabic',
+  fontWeight: FontWeight.w500,
+);
+
+const IBMPlexSansArabicBold = TextStyle(
+  fontFamily: 'IBMPlexSansArabic',
+  fontWeight: FontWeight.w700,
+);
+
+const IBMPlexSansArabicBlack = TextStyle(
+  fontFamily: 'IBMPlexSansArabic',
+  fontWeight: FontWeight.w900,
+);
